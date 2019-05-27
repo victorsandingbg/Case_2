36f89b3840291d2588efca84034c494a889faae2
Victor Sandin <victor.sandin@outlook.com>
2019-05-27 15:54:22 +0200

    hej

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
-var moment = require('moment');
+
 
 
 var indexRouter = require('./routes/index');
